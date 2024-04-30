7e6e03ddce53218c7971b8323fcf9f8ded9e6959
atul <atul.etech2011@gmail.com>
2024-04-30 10:51:39 +0000

    restore vite in frontend

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server:{
-  //   proxy:{
-  //     "/api":"http://116.202.210.102:6000",
-  //   }
-  // }
+  server:{host:true}
 })
